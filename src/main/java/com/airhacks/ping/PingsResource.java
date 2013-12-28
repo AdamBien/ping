d3a10d1a770bfa4bdc685119edb2af4f56e1b1b9
Adam Bien <abien@adam-bien.com>
2013-12-28 06:45:02 +0100

    @GET added to methods

--- src/main/java/com/airhacks/ping/PingsResource.java
+++ src/main/java/com/airhacks/ping/PingsResource.java
@@ -6,26 +6,24 @@ import java.io.File;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author adam-bien.com
  */
 @Path("pings")
-@Produces(MediaType.TEXT_PLAIN)
-@Consumes(MediaType.TEXT_PLAIN)
 public class PingsResource {
 
+    @GET
     @Path("/echo/{echo}")
     public String echo(@PathParam("echo") String param) {
         return param;
     }
 
+    @GET
     @Path("/system-properties")
     public String systemProperties() {
         StringBuilder retVal = new StringBuilder();
