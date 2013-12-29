f68aef79715c3a9cd85f8358e67d876245f82744
Adam Bien <abien@adam-bien.com>
2013-12-29 06:14:49 +0100

    JNDI listing is working

--- src/main/java/com/airhacks/ping/PingsResource.java
+++ src/main/java/com/airhacks/ping/PingsResource.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -31,6 +35,22 @@ public class PingsResource {
         Set<Map.Entry<Object, Object>> entries = properties.entrySet();
         for (Map.Entry<Object, Object> property : entries) {
             retVal.append(property.getKey()).append(" -> ").append(property.getValue());
+            retVal.append(File.separatorChar);
+        }
+        return retVal.toString();
+    }
+
+    @GET
+    @Path("/jndi/{namespace}")
+    public String jndi(@PathParam("namespace") String namespace) throws NamingException {
+        StringBuilder retVal = new StringBuilder();
+        InitialContext c = new InitialContext();
+        NamingEnumeration<NameClassPair> list = c.list(namespace);
+        while (list.hasMoreElements()) {
+            NameClassPair nameClassPair = list.nextElement();
+            String name = nameClassPair.getName();
+            String type = nameClassPair.getClassName();
+            retVal.append(name).append(" -> ").append(type);
             retVal.append(File.separatorChar);
         }
         return retVal.toString();
