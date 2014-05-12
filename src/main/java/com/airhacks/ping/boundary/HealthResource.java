f5baafa82d7f84b647e04d299afdd4ba5be1f37c
Adam Bien <abien@adam-bien.com>
2014-05-12 20:15:35 +0200

    Memory stats implemented

--- src/main/java/com/airhacks/ping/boundary/HealthResource.java
+++ src/main/java/com/airhacks/ping/boundary/HealthResource.java
@@ -2,6 +2,9 @@ package com.airhacks.ping.boundary;
 
 import com.airhacks.ping.control.ServerWatch;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,9 +22,18 @@ public class HealthResource {
     ServerWatch watch;
 
     @GET
-    @Path("/boot-time")
+    @Path("/start-time")
     @Produces(MediaType.TEXT_PLAIN)
     public String bootTime() {
         return this.watch.getDateTime().toString();
+    }
+
+    @GET
+    @Path("/current-memory")
+    public JsonObject availableHeap() {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("Available memory in mb", this.watch.availableMemoryInMB()).
+                add("Used memory in mb", this.watch.usedMemroyInMb());
+        return builder.build();
     }
 }
--- src/main/java/com/airhacks/ping/control/ServerWatch.java
+++ src/main/java/com/airhacks/ping/control/ServerWatch.java
@@ -1,5 +1,8 @@
 package com.airhacks.ping.control;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.time.ZonedDateTime;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -13,19 +16,40 @@ import javax.ejb.Startup;
 @Singleton
 public class ServerWatch {
 
-    private ZonedDateTime bootTime;
+    private ZonedDateTime startTime;
+    private MemoryUsage heapUsageAtStartTime;
+    private MemoryMXBean memoryMxBean;
 
     @PostConstruct
     public void initialize() {
-        this.setbootTime();
+        this.initializeStartTime();
+        this.memoryMxBean = ManagementFactory.getMemoryMXBean();
+        this.heapUsageAtStartTime = this.memoryMxBean.getHeapMemoryUsage();
+
     }
 
-    void setbootTime() {
-        this.bootTime = ZonedDateTime.now();
+    void initializeStartTime() {
+        this.startTime = ZonedDateTime.now();
     }
 
     public ZonedDateTime getDateTime() {
-        return this.bootTime;
+        return this.startTime;
+    }
+
+    public double availableMemoryInMB() {
+        MemoryUsage current = this.memoryMxBean.getHeapMemoryUsage();
+        long available = (current.getCommitted() - current.getUsed());
+        return asMb(available);
+    }
+
+    public double usedMemroyInMb() {
+        MemoryUsage current = this.memoryMxBean.getHeapMemoryUsage();
+        return asMb(current.getUsed());
+
+    }
+
+    double asMb(long bytes) {
+        return bytes / 1024 / 1024;
     }
 
 }
